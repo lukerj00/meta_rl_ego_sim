3d078afd7f219416ef6f5fad1b51aaa42c076bb6
lukerj00 <luke.johnston00@gmail.com>
2023-02-11 19:09:59 +0000

    11/02

--- csv_plotter/txt_plotter_final.py
+++ csv_plotter/txt_plotter_final.py
@@ -15,17 +15,16 @@ from datetime import datetime
 
 path_ = str(Path(__file__).resolve().parents[1])
 stdout_ = path_ + '\\stdout\\'
-dump_ = open(stdout_ + 'dump30.txt','r').read() # 41,30(s,r,d)
+dump_ = open(stdout_ + 'dump41.txt','r').read() # 41(s,d),30(s,r,d)
 # path_pkl = path_ + "\\pkl\\"
 # colors_ = open(path_pkl + colors_file,'rb')
 colors = np.float32([[255,100,50],[50,255,100],[100,50,255]])/255 # [[255,100,50],[50,255,100],[100,50,255],[200,0,50]]) # ,[# colors = pickle.load(colors_) # [r,g,b*5]
 DOTS = colors.shape[0]
 EPOCHS_ = [0,1000,2000]
-VMAPS = 3 # 3 4?
-IT = 25 # 20
+VMAPS = 1 # 3
+IT = 20 # 25
 
 ### txt to arrays (regex)
-
 ### sel
 sel = []
 for e in EPOCHS_:
@@ -34,7 +33,7 @@ for e in EPOCHS_:
     epoch_ = re.findall(epoch_reg0,dumpstr_,re.DOTALL)
     epoch_ = ' '.join(epoch_) # only one match
     epoch_ = re.sub(r'(\\|\=|\_)',' ',epoch_)
-    sel_reg = r'(?<=nsel)(.*)(?=tot)' # buggy end condition.. try /i
+    sel_reg = r'(?<=nsel)(.*)(?=ndis)' # (tot) buggy end condition.. try /i
     sel_ = re.findall(sel_reg,epoch_)
     sel_ = ''.join(sel_) # only one match
     sel_ = re.findall(r'\d+',sel_)
@@ -43,7 +42,7 @@ for e in EPOCHS_:
     sel_ = [sel_[i:i+DOTS] for i in range(0, DOTS*VMAPS, DOTS)]# sel_ = sel_.lstrip(r'\[')
     sel += sel_
 sel_arr = np.asarray(sel)
-# print('sel',sel_arr.shape)
+# print('sel',sel_arr.shape,sel_arr)
 
 ### dis
 dis = []
@@ -60,16 +59,15 @@ for e in EPOCHS_:
     dis_ = re.sub(r'(\[|\]|\\n|\=|dis)','',dis_) # removes [ ] \n =
     dis_ = dis_.split(' ') # split by space
     dis_ = list(filter(None, dis_))
-    # print('dis_',dis_)
     dis_ = dis_[:(DOTS*IT*VMAPS)] # max VMAP=4
     dis += dis_
 dis_arr = np.asarray(dis).reshape([-1,DOTS]).astype(np.float32) # [DOTS*IT,VMAPS]
 print('dis_arr',dis_arr.shape,dis_arr)
-e=2
-d=1
-v=1
-test = dis_arr[(e*(VMAPS*IT)+v*IT):(e*(VMAPS*IT)+(v+1)*IT-1),d]
-print('dis_arr',dis_arr.shape,dis_arr,'\n test',test) # e=2,d=1,v=1
+# e=2
+# d=1
+# v=1
+# test = dis_arr[(e*(VMAPS*IT)+v*IT):(e*(VMAPS*IT)+(v+1)*IT-1),d] # old
+# print('dis_arr',dis_arr.shape,dis_arr,'\n test',test) # e=2,d=1,v=1
 
 # ### dis
 # dis = []
@@ -96,13 +94,13 @@ print('dis_arr',dis_arr.shape,dis_arr,'\n test',test) # e=2,d=1,v=1
 
 ### plot
 fig, axis = plt.subplots(len(EPOCHS_),VMAPS)
+axis = axis.reshape(-1,VMAPS)
 for e in range(len(EPOCHS_)):
     for v in range(VMAPS):
             for d in range(DOTS):
-                sel_ = sel_arr[DOTS*v+d] ## CHANGE
-                # axis[e,v].plot(dis_arr[e*IT+((v)*IT):((v+1)*IT-1),d],color=tuple(colors[d,:]))
-                # print('e',e,'v',v,'d',d,'indices:',(e*(DOTS*IT)+d*DOTS),(e*(DOTS*IT)+(d+1)*DOTS-1),v)
-                axis[e,v].plot(dis_arr[(e*(VMAPS*IT)+d*IT):(e*(VMAPS*IT)+(d+1)*IT-1),v],color=tuple(colors[d,:]))
-                axis[e,v].set_title(f'epoch {EPOCHS_[e]}, select = {sel_}',fontsize=10)
+                sel_ = sel_arr[VMAPS*e+v] # [EPOCHS*VMAPS,]
+                print('e',e,'v',v,'d',d,'sel',sel_) # ,'indices:',(e*(DOTS*IT)+d*DOTS),(e*(DOTS*IT)+(d+1)*DOTS-1),v)
+                axis[e,v].plot(dis_arr[(e*(VMAPS*IT)+v*IT):(e*(VMAPS*IT)+(v+1)*IT-1),d],color=tuple(colors[d,:]))
+                axis[e,v].set_title(f'epoch {EPOCHS_[e]}, select = {sel_}',fontsize=8)
 fig.tight_layout(pad=1.5)
 plt.show()
